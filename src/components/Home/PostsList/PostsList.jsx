4610a27002f6143e4b7817f790f6d7517b8f6f8a
Nguyen Hoang Anh <anhnh@enjoysport.vn>
2024-10-26 11:48:21 +0700

    save point

---
+++ src/components/Home/PostsList/PostsList.jsx
@@ -0,0 +1,22 @@
+export default function PostsList() {
+  const datas = [
+    {
+      img: "",
+      name: "Elon Musk",
+      content:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      images: [],
+      likeCount: 241000,
+      commentCount: 45,
+    },
+    {
+      img: "",
+      name: "HYPEBEAST",
+      content: "Sole Mates: Ralph Suguitan and the Nike KD 4 *Easter",
+      images: [],
+      likeCount: 1000,
+      commentCount: 30,
+    },
+  ];
+  return <p>post list</p>;
+}
