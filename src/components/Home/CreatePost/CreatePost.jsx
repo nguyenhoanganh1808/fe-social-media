51e4cac3e8ea757347f7ca04aebf33779a4a5f86
Nguyen Hoang Anh <82761891+nguyenhoanganh1808@users.noreply.github.com>
2024-11-07 08:55:24 +0700

    Feature: show create post modal

--- src/components/Home/CreatePost/CreatePost.jsx
+++ src/components/Home/CreatePost/CreatePost.jsx
@@ -1,8 +1,17 @@
+import { useRef } from "react";
 import styles from "./CreatePost.module.css";
+import CreatePostModal from "./CreatePostModal/CreatePostModal";
 
 export default function CreatePost() {
+  const dialogRef = useRef(null);
+
+  function handleTextAreaClick() {
+    dialogRef.current?.showModal();
+  }
+
   return (
     <div className={styles.container}>
+      <CreatePostModal ref={dialogRef} />
       <img
         className={styles.avatar}
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStqtktl3g6wWkAzvUAi32yzYgb-jZ0-Pn0sQ&s"
@@ -11,6 +20,7 @@ export default function CreatePost() {
 
       <div className={styles.postContainer}>
         <textarea
+          onClick={handleTextAreaClick}
           className={styles.postContent}
           name="content"
           id="content"
---
+++ src/components/Home/CreatePost/CreatePostModal/CreatePostModal.jsx
@@ -0,0 +1,14 @@
+import { forwardRef } from "react";
+
+const CreatePostModal = forwardRef(function CreatePostModal(props, ref) {
+  return (
+    <dialog ref={ref} open>
+      <p>Greetings, one and all!</p>
+      <form method="dialog">
+        <button>OK</button>
+      </form>
+    </dialog>
+  );
+});
+
+export default CreatePostModal;
