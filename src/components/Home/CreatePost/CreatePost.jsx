5aaacb3585bfe4e8e144e468ec084e14606a3e50
Nguyen Hoang Anh <82761891+nguyenhoanganh1808@users.noreply.github.com>
2024-11-08 17:37:01 +0700

    save point

--- src/components/Home/CreatePost/CreatePost.jsx
+++ src/components/Home/CreatePost/CreatePost.jsx
@@ -4,6 +4,12 @@ import CreatePostModal from "./CreatePostModal/CreatePostModal";
 
 export default function CreatePost() {
   const dialogRef = useRef(null);
+  const author = {
+    name: "George Jose",
+    link: "george",
+    avatarUrl:
+      "https://s3-alpha-sig.figma.com/img/d6a6/fffd/2100d987b5946aa1df33cf0f6c34a418?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=p0ozEGQKCEzpMDFiGB0WI7e3Jy5hs~XAzkQEmXckRff9Yql3RG4oZ3qz7U-lmzlgN5l~VSp88OF3WSEHLG8hewYzujk8BfcrkQvmBJSeDEvGfDm6uDq5Hsx9cQ1cfgLQHZz10fHEBoS7H8kb73P-hWtJj2qYrDYGdR5YhEHfHT9EQehf5C90C4PZkDVQRu6FZWCVcxHr7FcTZGWaNC-LSjOSmA4KZaKs1FIk-OEuIlV9fK7KSQoo-7pSIHsF2dGTfhxvY4s8ywp-UY0qcUPSa7BHmFJKLlpd~NhfU2JEvdZGid5XK-F6O7RbD4CLUF1cc6yaUuh1WrCdQ9fM~aRMJQ__",
+  };
 
   function handleTextAreaClick() {
     dialogRef.current?.showModal();
@@ -11,7 +17,7 @@ export default function CreatePost() {
 
   return (
     <div className={styles.container}>
-      <CreatePostModal ref={dialogRef} />
+      <CreatePostModal author={author} ref={dialogRef} />
       <img
         className={styles.avatar}
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStqtktl3g6wWkAzvUAi32yzYgb-jZ0-Pn0sQ&s"
--- src/components/Home/CreatePost/CreatePostModal/CreatePostModal.jsx
+++ src/components/Home/CreatePost/CreatePostModal/CreatePostModal.jsx
@@ -1,14 +1,42 @@
 import { forwardRef } from "react";
+import styles from "./CreatePostModal.module.css";
+import PropTypes from "prop-types";
+import { Smile, Image, File, Link } from "lucide-react";
 
-const CreatePostModal = forwardRef(function CreatePostModal(props, ref) {
+const CreatePostModal = forwardRef(function CreatePostModal({ author }, ref) {
   return (
-    <dialog ref={ref}>
-      <p>Greetings, one and all!</p>
-      <form method="dialog">
-        <button>OK</button>
-      </form>
+    <dialog className={styles.wrapper} ref={ref}>
+      <h1>Create Post</h1>
+      <hr />
+      <div>
+        <img className={styles.avatar} src={author.avatarUrl} alt="" />
+        <div>
+          <p>{author.link}</p>
+          <p>{author.name}</p>
+        </div>
+      </div>
+
+      <textarea name="" id=""></textarea>
+      <Smile />
+      <div>
+        <p>Add to your post</p>
+        <div>
+          <Image />
+          <File />
+          <Link />
+        </div>
+      </div>
+      <button>Post</button>
     </dialog>
   );
 });
+
+CreatePostModal.propTypes = {
+  author: PropTypes.shape({
+    avatarUrl: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }),
+};
 
 export default CreatePostModal;
