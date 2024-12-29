cd802babde7d0f73cfdba90f538537496d34da96
nguyenhoanganh1808 <1808nguyenhoanganh2003@gmail.com>
2024-12-29 15:33:16 +0700

    feat: change avatar

--- src/components/Profile/AvatarContainer/AvatarContainer.jsx
+++ src/components/Profile/AvatarContainer/AvatarContainer.jsx
@@ -6,9 +6,37 @@ import styles from "./AvatarContainer.module.css";
 import PropTypes from "prop-types";
 import { formatNumber } from "../../../lib/utils";
 import { useAuth } from "../../../hooks/useAuthContext";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import ProfileService from "../../../services/profile.service";
 
 export default function AvatarContainer({ userData }) {
-  const { user } = useAuth();
+  const { user, setUser } = useAuth();
+  const [avatarUrl, setAvatarUrl] = useState(user.avatarUrl);
+
+  const handleAvatarUrl = async (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const response = await toast.promise(
+      ProfileService.updateProfileImage("AVATAR", file),
+      {
+        pending: "Uploading avatar photo...",
+        success: "Avatar photo uploaded successfully 👌",
+        error: "Failed to upload avatar photo 🤯",
+      }
+    );
+
+    if (response.error) {
+      toast.error(response.error);
+      return;
+    }
+
+    const url = URL.createObjectURL(file);
+    setAvatarUrl(url);
+    setUser((prevUser) => ({ ...prevUser, avatarUrl: url }));
+  };
+
   return (
     <div className={styles.avatarContainer}>
       <div className={styles.followContainer}>
@@ -16,13 +44,14 @@ export default function AvatarContainer({ userData }) {
         <p>Followers</p>
       </div>
       <div className={styles.avatar}>
-        <Avatar src={user.avatarUrl} size={130} />
+        <Avatar src={avatarUrl} size={130} />
         <label htmlFor="avatarFile">
           <LucideCircleButton size={40}>
             <Camera fill="gray" color="white" />
           </LucideCircleButton>
         </label>
         <input
+          onChange={handleAvatarUrl}
           className={styles.avatarInput}
           type="file"
           id="avatarFile"
