e44be20f56aa1f8d1aadde13642265ae1f5ecbcc
nguyenhoanganh1808 <1808nguyenhoanganh2003@gmail.com>
2025-02-13 11:02:07 +0700

    fix: delete follow and message btn in profile

--- src/pages/Profile/ProfilePage.jsx
+++ src/pages/Profile/ProfilePage.jsx
@@ -2,17 +2,17 @@ import styles from "./ProfilePage.module.css";
 import CoverPhoto from "../../components/Profile/CoverPhoto/CoverPhoto";
 import AvatarContainer from "../../components/Profile/AvatarContainer/AvatarContainer";
 import NavBar from "../../components/Profile/NavBar/NavBar";
-import { Link, Outlet, useParams } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import ProfileService from "../../services/profile.service";
 import Spinner from "../../components/common/Spinner/Spinner";
-import LoadingButton from "../../components/common/Spinner/LoadingButton";
-import { useAuth } from "../../hooks/useAuthContext";
+// import LoadingButton from "../../components/common/Spinner/LoadingButton";
+// import { useAuth } from "../../hooks/useAuthContext";
 
 export default function ProfilePage() {
   const { id } = useParams();
   const [userInfo, setUserInfo] = useState(null);
-  const { user } = useAuth();
+  // const { user } = useAuth();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -44,14 +44,14 @@ export default function ProfilePage() {
           <p>{userInfo.nickName}</p>
           <p>@{userInfo.tagName}</p>
         </div>
-        {user.userId !== id && (
+        {/* {user.userId !== id && (
           <div className="flex justify-center m-3">
             <LoadingButton>Follow</LoadingButton>
             <Link to={`/message/${id}`}>
               <LoadingButton>Message</LoadingButton>
             </Link>
           </div>
-        )}
+        )} */}
         <NavBar />
       </div>
       <div className={styles.downContainer}>
